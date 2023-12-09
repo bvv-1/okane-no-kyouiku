f0b45a12b2faf8de2748d17432f96a8201de5d10
bvv-1 <fhinnoigsr@gmail.com>
2023-12-09 23:52:38 +0900

    app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import { useState, useEffect } from "react"
 import { getHelloWorldApi } from "./utils/links"
+import "./App.css"
 
-function App() {
+export default function App() {
+  const [uiState, setUIState] = useState<UIState>(UIState.Start)
   const [data, setData] = useState("")
 
   useEffect(() => {
     const fetchHelloWorld = async () => {
-      const response = await fetch(getHelloWorldApi)
+      const response = await fetch(getHelloWorldApi())
 
       if (!response.ok) {
         throw new Error(`HTTP error! Status: ${response.status}`)
@@ -20,10 +22,43 @@ function App() {
   }, [])
 
   return (
-    <>
+    <div className="App">
       {data}
-    </>
+      <button onClick={() => setUIState(UIState.Start)}><Start /></button>
+      <button onClick={() => setUIState(UIState.Plan)}><Plan /></button>
+      <button onClick={() => setUIState(UIState.Record)}><Record /></button>
+      <button onClick={() => setUIState(UIState.Progress)}><Progress /></button>
+
+      {uiState === UIState.Start && <div>Start</div>}
+      {uiState === UIState.Plan && <div>Plan</div>}
+      {uiState === UIState.Record && <div>Record</div>}
+      {uiState === UIState.Progress && <div>Progress</div>}
+    </div>
   )
 }
 
-export default App
+// ここからをメインでいじってください!
+function Start() {
+  return <div>Start</div>
+}
+
+function Plan() {
+  return <div>Plan</div>
+}
+
+function Record() {
+  return <div>Record</div>
+}
+
+function Progress() {
+  return <div>Progress</div>
+}
+// ここまでをメインでいじってください!
+
+const UIState = {
+  Start: 0,
+  Plan: 1,
+  Record: 2,
+  Progress: 3,
+} as const
+type UIState = typeof UIState[keyof typeof UIState]
