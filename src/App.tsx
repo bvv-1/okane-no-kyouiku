d986dd01cf5318f01d776059390e18ec191e8e1c
bvv-1 <fhinnoigsr@gmail.com>
2023-12-09 21:01:04 +0900

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,28 @@
+import { useState, useEffect } from "react"
+
 function App() {
+  const API_BASE_URL = "https://flask-okane-no-kyouiku.onrender.com/" // "http://127.0.0.1:5000/""
+
+  const [data, setData] = useState("")
+
+  useEffect(() => {
+    const fetchHelloWorld = async () => {
+      const response = await fetch(API_BASE_URL)
+
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`)
+      }
+
+      const jsonData = await response.json()
+      setData(jsonData["message"])
+    }
+    
+    fetchHelloWorld()
+  }, [])
+
   return (
     <>
-      hello
+      {data}
     </>
   )
 }
