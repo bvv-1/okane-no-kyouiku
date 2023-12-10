f1cc8a84a4901bc339c99b105e3c1bfb8137f79f
yukiringo_kkk <yuki@yukiringo-kkknoMacBook-Air.local>
2023-12-10 17:56:26 +0900

    css

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,7 @@ export default function App() {
   return (<>
 
     <link rel="preconnect" href="https://fonts.googleapis.com" />
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin></link>
+    <link rel="preconnect" href="https://fonts.gstatic.com" />
     <link href="https://fonts.googleapis.com/css2?family=Yusei+Magic&display=swap" rel="stylesheet"></link>
     <div className="App">
       <div className="navi">
