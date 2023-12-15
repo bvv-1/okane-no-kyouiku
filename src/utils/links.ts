7b3f6311b6fb25c9b2681232809723ac7d448a1d
bvv-1 <fhinnoigsr@gmail.com>
2023-12-16 01:20:42 +0900

    Update links.ts

--- src/utils/links.ts
+++ src/utils/links.ts
@@ -1,4 +1,4 @@
-const API_BASE_URL = "https://gin-okane-no-kyouiku-api.onrender.com"
+const API_BASE_URL = "https://gin-okane-no-kyouiku-api.onrender.com/"
 
 export const getHelloWorldApi = () => {
   return `${API_BASE_URL}`
