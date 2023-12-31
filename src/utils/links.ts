4fc80421834705c1bdfe15e9e63e99ea3aa2b18c
bvv-1 <fhinnoigsr@gmail.com>
2023-12-31 16:58:43 +0900

    fix: render env vars

--- src/utils/links.ts
+++ src/utils/links.ts
@@ -1,4 +1,4 @@
-const API_BASE_URL = "http://localhost:8080/" // "https://gin-okane-no-kyouiku-api.onrender.com/"
+const API_BASE_URL = "https://gin-okane-no-kyouiku-api.onrender.com/"
 
 export const getHelloWorldApi = () => {
   return `${API_BASE_URL}`
